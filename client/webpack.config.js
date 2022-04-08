5b48c88191de26364e4c4d742bf80832df228fe0
Tracye Wilhelm <93944059+tracyewilhelm@users.noreply.github.com>
2022-04-07 18:35:21 -0600

    try try again

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,11 +1,14 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 module.exports = () => {
   return {
     mode: "development",
     entry: {
-      main: "./assets/js/index.js",
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
       filename: "[name].bundle.js",
