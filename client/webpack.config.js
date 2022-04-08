bcb0cfd1504d30382a0861917101f7abb33f395c
Tracye Wilhelm <93944059+tracyewilhelm@users.noreply.github.com>
2022-04-07 20:13:01 -0600

    starting again

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -17,6 +17,26 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: "./index.html",
+        name: "My Progressive Web App",
+        short_name: "MyPWA",
+        description: "My awesome Progressive Web App!",
+        background_color: "#ffffff",
+        crossorigin: "use-credentials",
+        icons: [
+          {
+            src: path.resolve("src/assets/icon.png"),
+            sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
+          },
+          {
+            src: path.resolve("src/assets/large-icon.png"),
+            size: "1024x1024", // you can also use the specifications pattern
+          },
+          {
+            src: path.resolve("src/assets/maskable-icon.png"),
+            size: "1024x1024",
+            purpose: "maskable",
+          },
+        ],
       }),
     ],
 
